1bd7d8ac5041e7de376a01f428784a4f2201b4b7
Imran Khan <khalekuzzamanimran@gmail.com>
2023-12-05 22:10:15 +0600

    function-components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-// Class Components
+// Function components
 
-import React from "react";
-
-class Heading extends React.Component {
-  render() {
-    return <h1>Hi, My name is Imran.</h1>;
-  }
+function Heading() {
+  return <h1>Hi, My name is Imran.</h1>;
 }
 
 export default Heading;
