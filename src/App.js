4658b32385c9f982d6dfe8ab83c0e4efa8af5242
Imran Khan <khalekuzzamanimran@gmail.com>
2023-12-05 22:49:09 +0600

    class-components-with-state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-// Function components
+// Class components with constructor
+import React from "react";
 
-function Heading() {
-  return <h1>Hi, My name is Imran.</h1>;
+class Car extends React.Component {
+  constructor() {
+    super();
+    this.state = { color: "Red", brand: "Ford", model: "Mustang", year: 1964 };
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Hi, I am a {this.state.brand} Car!</h2>
+        <p>
+          It is a {this.state.color} {this.state.model}
+          from {this.state.year}
+        </p>
+      </div>
+    );
+  }
 }
 
-export default Heading;
+export default Car;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.css";
-import Heading from "./App";
+import Car from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Heading />);
+root.render(<Car />);
